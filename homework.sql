10e4684a8e1514b219cff3950c7dc7e93f36c133
kalaiyarasan072 <kalaiarasanmba5@gmail.com>
2026-06-04 12:28:04 +0530

    Add files via upload

---
+++ homework.sql
@@ -0,0 +1,9 @@
+SELECT * from countries
+
+Create table countries (country_name varchar(20), total_domain varchar2(20));
+
+insert all
+insert into countries values('chinna', '7');
+insert into countries values('india', '6');
+insert into countries values('colombia', '9');
+select * from dual;
