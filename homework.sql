ed08fb8e7efb29a711d4211e8dfec7b7e02f1703
kalaiyarasan072 <kalaiarasanmba5@gmail.com>
2026-06-06 10:39:03 +0530

    Delete homework.sql

--- homework.sql
+++
@@ -1,9 +0,0 @@
-SELECT * from countries
-
-Create table countries (country_name varchar(20), total_domain varchar2(20));
-
-insert all
-insert into countries values('chinna', '7');
-insert into countries values('india', '6');
-insert into countries values('colombia', '9');
-select * from dual;
